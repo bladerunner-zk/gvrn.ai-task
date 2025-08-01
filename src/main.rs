cc56bd6b938034b6f461237d0e683184a3bc1887
Stanisław Durka <stas.durka@gmail.com>
2025-08-01 12:30:23 +0200

    connect to grpc endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use log::{error, info, warn};
+use yellowstone_grpc_client::GeyserGrpcClient;
+use tonic::service::Interceptor;
+
+const GRPC_ENDPOINT: &str = "http://134.119.192.123:10000";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+    info!("Connecting to gRPC enpoint at {}", GRPC_ENDPOINT);
+    let mut client = setup_client().await?;
+    info!("Connected to gRPC endpoint");
+    Ok(())
+}
+
+async fn setup_client() -> Result<GeyserGrpcClient<impl Interceptor>, Box<dyn std::error::Error>> {    
+    let client = GeyserGrpcClient::build_from_shared(GRPC_ENDPOINT.to_string())?
+        .connect()
+        .await?;
+    
+    Ok(client)
 }
