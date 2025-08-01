c2ff65cb384e9f6bc9c950292d6043ab981bf6aa
Stanisław Durka <stas.durka@gmail.com>
2025-08-01 16:16:48 +0200

    Pump.fun token launch detection

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,11 @@ use yellowstone_grpc_proto::{
 const GRPC_ENDPOINT: &str = "http://134.119.192.123:10000";
 const RAYDIUM_LAUNCHLAB_PROGRAM: &str = "LanMV9sAd7wArD4vJFi2qDdfnVhFxYSUg6eADduJ3uj";
 const LAUNCHLAB_INITIALIZE_DISCRIMINATOR: &[u8] = &[175, 175, 109, 31, 13, 152, 155, 237];
+const RAYDIUM_INIT_IDX: usize = 6;
+
+const PUMP_FUN_PROGRAM: &str = "6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P";
+const PUMP_FUN_CREATE_DISCRIMINATOR: &[u8] = &[24, 30, 200, 40, 5, 28, 7, 119];
+const PUMP_FUN_INIT_IDX: usize = 6;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -65,6 +70,18 @@ where
             vote: Some(false),
             failed: Some(false),
             signature: None,
+        });
+    accounts_filter.insert(
+        "Pump.fun monitor".to_string(),
+        SubscribeRequestFilterTransactions {
+            account_include: vec![],
+            account_exclude: vec![],
+            account_required: vec![
+                PUMP_FUN_PROGRAM.to_string(),
+            ],
+            vote: Some(false),
+            failed: Some(false),
+            signature: None,
         },
     );
     tx.send(SubscribeRequest {
@@ -103,6 +120,12 @@ fn handle_message(msg: SubscribeUpdate) {
                     if let Some(message) = &tx.message {
                         for instruction in &message.instructions {
                             let program_id = bs58::encode(&message.account_keys[instruction.program_id_index as usize]).into_string();
+
+                            if program_id == PUMP_FUN_PROGRAM && instruction.data.starts_with(&PUMP_FUN_CREATE_DISCRIMINATOR) {
+                                let base_mint_index = instruction.accounts[6] as usize;
+                                let base_mint_pubkey = &message.account_keys[base_mint_index];
+                                info!("Pump.fun token created! CA: {}", bs58::encode(base_mint_pubkey).into_string());
+                            }
 
                             if program_id == RAYDIUM_LAUNCHLAB_PROGRAM && instruction.data.starts_with(&LAUNCHLAB_INITIALIZE_DISCRIMINATOR) {
                                 let base_mint_index = instruction.accounts[6] as usize;
